7a76948a54b22eb8247cea8799a6398b868f284c
Gemma <gcarravi@gmail.com>
2026-03-04 20:08:50 +0000

    changing to whitenoise.storage.CompressedStaticFilesStorage

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -150,7 +150,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
