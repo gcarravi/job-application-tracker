11b6ddb10ffd71ad72114994371b79e4e4db614d
Gemma <gcarravi@gmail.com>
2026-03-07 19:17:19 +0000

    integrated tracker within the new look and feel of the app

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -151,7 +151,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+if DEBUG:
+    STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+else:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
